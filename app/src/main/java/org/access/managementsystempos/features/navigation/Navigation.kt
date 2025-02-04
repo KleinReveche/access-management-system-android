8e2df751b6342f491f858b86217c24c52e2779b4
Klein Reveche <therealklr2003@gmail.com>
2025-02-04 16:23:59 +0800

    Add Settings

--- app/src/main/java/org/access/managementsystempos/features/navigation/Navigation.kt
+++ app/src/main/java/org/access/managementsystempos/features/navigation/Navigation.kt
@@ -13,6 +13,8 @@ import org.access.managementsystempos.features.main.MainScreen
 import org.access.managementsystempos.features.main.MainScreenDestination
 import org.access.managementsystempos.features.pos.POSScreen
 import org.access.managementsystempos.features.pos.POSScreenDestination
+import org.access.managementsystempos.features.settings.SettingsDestination
+import org.access.managementsystempos.features.settings.SettingsScreen
 
 @Composable
 fun Navigation() {
@@ -67,6 +69,22 @@ fun Navigation() {
             }
         ) {
             POSScreen()
+        }
+        composable<SettingsDestination>(
+
+            enterTransition = {
+                return@composable fadeIn(tween(1000))
+            }, exitTransition = {
+                return@composable slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Start, tween(700)
+                )
+            }, popEnterTransition = {
+                return@composable slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.End, tween(700)
+                )
+            }
+        ) {
+            SettingsScreen()
         }
     }
 }
---
+++ app/src/main/java/org/access/managementsystempos/features/settings/Settings.kt
@@ -0,0 +1,13 @@
+package org.access.managementsystempos.features.settings
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import kotlinx.serialization.Serializable
+
+@Composable
+fun SettingsScreen() {
+    Text("Settings")
+}
+
+@Serializable
+object SettingsDestination
