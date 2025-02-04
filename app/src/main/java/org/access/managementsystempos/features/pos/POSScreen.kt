2bf042d4c065f723365a9727c0adab3bd1b0b7ce
Klein Reveche <therealklr2003@gmail.com>
2025-02-04 21:40:11 +0800

    Add Scaffold to Base Screen

--- app/src/main/java/org/access/managementsystempos/features/pos/POSScreen.kt
+++ app/src/main/java/org/access/managementsystempos/features/pos/POSScreen.kt
@@ -1,5 +1,9 @@
 package org.access.managementsystempos.features.pos
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -7,7 +11,15 @@ import kotlinx.serialization.Serializable
 
 @Composable
 fun POSScreen() {
-    Text("POS Screen")
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text("POS Screen")
+        }
+    }
 }
 
 @Serializable
--- app/src/main/java/org/access/managementsystempos/features/settings/Settings.kt
+++ app/src/main/java/org/access/managementsystempos/features/settings/Settings.kt
@@ -1,12 +1,25 @@
 package org.access.managementsystempos.features.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import kotlinx.serialization.Serializable
 
 @Composable
 fun SettingsScreen() {
-    Text("Settings")
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text("Settings")
+        }
+    }
 }
 
 @Serializable
